809aa7dc33cfb01890e7ffdfe1785de6139fd628
hunar-1997 <hbkurd@gmail.com>
2016-12-25 10:58:26 +0300

    fixed a ton of bugs

--- app/src/main/java/com/hbkurd/dahat/History.java
+++ app/src/main/java/com/hbkurd/dahat/History.java
@@ -9,16 +9,19 @@ import android.widget.*;
 import android.content.*;
 import java.util.*;
 import android.text.*;
+import android.widget.AdapterView.*;
 
 public class History extends Activity
 {
 	functions fn = new functions();
 	
-	ListAdapter adapter;
 	List<List<Integer>> data;
+	List<String> content;
+	ArrayAdapter adapter;
 	
 	Button showDayButton;
-	Button right,left;
+	ListView historyList;
+	TextView dayText;
 	
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -26,22 +29,41 @@ public class History extends Activity
 		setContentView(R.layout.history);
 		
 		String h = getIntent().getStringExtra("data");
-		ListView list = (ListView) findViewById(R.id.historyList);
+		historyList = (ListView) findViewById(R.id.historyList);
 		data = fn.decode(h);
-
-		adapter = new ArrayAdapter<Integer>(this, R.layout.list, R.id.holder, fn.getList(data,0));
-		list.setAdapter(adapter);
 		
-		TextView day=(TextView)findViewById(R.id.dayId);
-		day.setText("تۆماری " + fn.parseDate( data.get(0).get(0) ) );
+		content = new ArrayList<String>();
+		adapter = new ArrayAdapter<String>(this, R.layout.list, R.id.holder, content);
+		
+		if(data.size()>0)
+			for(int i=0;i<data.size();i++)
+				content.add(fn.parseDate(data.get(i).get(0)));
+		else
+			content.add("تۆمار بەتاڵە");
+		
+		historyList.setAdapter(adapter);
+		
+		dayText=(TextView)findViewById(R.id.dayId);
+		dayText.setText("ڕۆژێک هەڵبژێرە" );
 		
 		showDayButton = (Button) findViewById(R.id.showDays);
-		right = (Button) findViewById(R.id.right);
-		left = (Button) findViewById(R.id.left);
 		
 		showDayButton.setVisibility(Button.INVISIBLE);
-		right.setVisibility(Button.INVISIBLE);
-		left.setVisibility(Button.INVISIBLE);
+		
+		historyList.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> l, View v, int position, long id)
+				{
+					if(data.get(position).size()>0){
+						content.clear();
+						adapter.notifyDataSetChanged();
+						for(int i=1;i<data.get(position).size();i++){
+							content.add(data.get(position).get(i).toString());
+						}
+						adapter.notifyDataSetChanged();
+						
+					}
+				}
+			});
 	}
 	
 	public void back(View view){
